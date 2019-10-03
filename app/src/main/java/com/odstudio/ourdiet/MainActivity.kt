aa3f2822396be2698ccb2ce0cd98283283a5e8d9
Austin <yo861015@gmail.com>
2019-10-03 17:21:26 +0800

    Update Both Navigation
    Add Navigation Drawer
    Add Bottom Navigation
    ---------------------------
    Bug Found
    When Clicking Drawer
    then Clicking Bottom Navigation
    The Drawer Will Stuck at the Last Click

--- app/src/main/java/com/odstudio/ourdiet/MainActivity.kt
+++ app/src/main/java/com/odstudio/ourdiet/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.odstudio.ourdiet
 
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBar
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
@@ -10,29 +12,55 @@ import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var mDrawerLayout: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
-        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
-        val navView: NavigationView = findViewById(R.id.nav_view)
-        val navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.nav_gallery, R.id.nav_slideshow, R.id.nav_tools, R.id.nav_share, R.id.nav_send
-            ), drawerLayout
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+        val actionbar: ActionBar? = supportActionBar
+        actionbar?.apply {
+            setDisplayHomeAsUpEnabled(true)
+            setHomeAsUpIndicator(R.drawable.baseline_menu_white_24dp)
+        }
+        mDrawerLayout = findViewById(R.id.drawer_layout)
+        val navigationView: NavigationView = findViewById(R.id.nav_view)
+        navigationView.setNavigationItemSelectedListener { menuItem ->
+            // set item as selected to persist highlight
+            menuItem.isChecked = true
+            // close drawer when item is tapped
+            mDrawerLayout.closeDrawers()
+
+            // Handle navigation view item clicks here.
+            when (menuItem.itemId) {
+
+                R.id.nav_gallery -> {
+                    findNavController(R.id.nav_host_fragment).navigate(R.id.nav_gallery)
+                }
+                R.id.nav_slideshow -> {
+                    findNavController(R.id.nav_host_fragment).navigate(R.id.nav_slideshow)
+                }
+                R.id.nav_tools -> {
+                    findNavController(R.id.nav_host_fragment).navigate(R.id.nav_tools)
+                }
+                R.id.nav_share -> {
+                    findNavController(R.id.nav_host_fragment).navigate(R.id.nav_share)
+                }
+                R.id.nav_send -> {
+                    findNavController(R.id.nav_host_fragment).navigate(R.id.nav_send)
+                }
+            }
+            true
+        }
         val bottomNavView: BottomNavigationView = findViewById(R.id.navigation)
         bottomNavView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
 
@@ -44,9 +72,16 @@ class MainActivity : AppCompatActivity() {
         //return true
    // }
 
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment)
-        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    //appbar - toolbar button click
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                mDrawerLayout.openDrawer(GravityCompat.START)
+                true
+            }
+
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 
     private val mOnNavigationItemSelectedListener =
