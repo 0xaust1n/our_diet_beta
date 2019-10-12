c79b1bffd2a230e20a02a4c0017d6ec50280e0cd
gamalu0527 <54342172+gamalu0527@users.noreply.github.com>
2019-10-12 17:30:09 +0800

    個人資料讀取
    頭像下載仍然有問題，待修復。

--- app/src/main/java/com/odstudio/ourdiet/PersonalActivity.kt
+++ app/src/main/java/com/odstudio/ourdiet/PersonalActivity.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.app.DatePickerDialog
 import android.content.Intent
 import android.net.Uri
+import android.nfc.Tag
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
@@ -18,6 +19,12 @@ import com.google.firebase.storage.FirebaseStorage
 import com.odstudio.ourdiet.Data_Class.User2
 import kotlinx.android.synthetic.main.activity_personal.uploadButton
 import java.util.*
+import com.bumptech.glide.Glide
+import  com.bumptech.glide.request.RequestListener
+import android.graphics.Bitmap
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.target.Target
 
 class PersonalActivity : AppCompatActivity() {
     // Global Declare
@@ -77,6 +84,7 @@ class PersonalActivity : AppCompatActivity() {
         birthdayText.setOnClickListener {
             dpd.show()
         }
+
         // upload Photo
         var upload = findViewById<Button>(R.id.uploadButton)
         upload.setOnClickListener {
@@ -88,9 +96,16 @@ class PersonalActivity : AppCompatActivity() {
         // Send Button
         var send = findViewById<Button>(R.id.btnPersonSend)
         send.setOnClickListener {
-                uploadImageToFirebaseStorage()
-                saveUserToFirebaseDatabase()
+            uploadImageToFirebaseStorage()
+            saveUserToFirebaseDatabase()
+            backtohome()
+            Toast.makeText(this, "已更新用戶資料", Toast.LENGTH_SHORT).show()
         }
+
+    }
+    override fun onResume() {
+        super.onResume()
+        getUserFromFirebaseDatabase()
     }
 
     private var selectedPhotoUri: Uri? = null
@@ -146,4 +161,90 @@ class PersonalActivity : AppCompatActivity() {
         FirebaseFirestore.getInstance().collection("Users").document(uid).set(account, SetOptions.merge())
     }
 
+    private fun backtohome(){
+        val intent = Intent(this, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        startActivity(intent)
+    }
+
+    private fun getUserFromFirebaseDatabase() {
+        val uid = FirebaseAuth.getInstance().currentUser!!.uid
+        var et_birthday = findViewById<EditText>(R.id.et_birthday)
+        var et_weight = findViewById<EditText>(R.id.et_weight)
+        var et_height = findViewById<EditText>(R.id.et_height)
+        var et_nick = findViewById<EditText>(R.id.et_nickname)
+
+        var avatarView =
+            findViewById<de.hdodenhof.circleimageview.CircleImageView>(R.id.uploadView)
+        val filename = FirebaseAuth.getInstance().currentUser!!.uid + "_avatar"
+        var avatarRef= FirebaseStorage.getInstance().getReference("/images/$filename").downloadUrl.addOnSuccessListener {
+
+            Glide.with(applicationContext)
+                .asBitmap()
+                .load(it)
+                .listener(object : RequestListener<Bitmap> {
+                    override fun onResourceReady(
+                        resource: Bitmap?,
+                        model: Any?,
+                        target: Target<Bitmap>?,
+                        dataSource: DataSource?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        avatarView.setImageBitmap(resource)
+                        return false;
+                    }
+
+                    override fun onLoadFailed(
+                        e: GlideException?,
+                        model: Any?,
+                        target: Target<Bitmap>?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        Toast.makeText(applicationContext, "取得頭像圖檔發生錯誤:"+e?.message, Toast.LENGTH_SHORT).show()
+
+                        return false;
+                    }
+                }
+                ).submit()
+
+        }
+
+        FirebaseFirestore.getInstance().collection("Users").whereEqualTo("uid",uid).get().addOnSuccessListener {
+            it.forEach {
+                et_nick.apply{
+                    setText(it.get("nick").toString())
+                }
+                et_birthday.apply {
+                    setText(it.get("birthday").toString())
+                }
+                et_weight.apply {
+                    setText(it.get("weight").toString())
+                }
+                et_height.apply {
+                    setText(it.get("height").toString())
+                }
+                val filename = FirebaseAuth.getInstance().currentUser!!.uid + "_avatar"
+                FirebaseStorage.getInstance().getReference("/images/$filename").downloadUrl.addOnSuccessListener { itUri ->
+                    Toast.makeText(this, "頭像圖檔位置"+itUri.path, Toast.LENGTH_LONG).show()
+
+                }.addOnFailureListener {  exception ->
+                    Toast.makeText(this, "取得頭像圖檔發生錯誤:"+exception.message, Toast.LENGTH_SHORT).show()
+
+               }
+
+            }
+        }
+
+
+        //Using DataClass push to Firebase Database
+        /*
+        val avatar = "/images/"+FirebaseAuth.getInstance().currentUser!!.uid + "_avatar"
+
+        val nick = et_nick!!.text.toString()
+        val birthday = et_birthday!!.text.toString()
+        val weight:Int = Integer.parseInt(et_weight!!.text.toString())
+        val height =Integer.parseInt( et_height!!.text.toString())
+        val bmi : Double  = (weight / (height*height/10000)).toDouble()
+        val account =User2(avatar,nick,birthday,height,weight,bmi,genderText)*/
+    }
 }
