3a0122366d9a462c38d74638d5d77facf1b1a8fd
gamalu__0527 <54342172+gamalu0527@users.noreply.github.com>
2019-11-05 22:49:18 +0800

    update foodfragment
    create recordbutton

--- app/src/main/java/com/odstudio/ourdiet/ui/food/FoodFragment.kt
+++ app/src/main/java/com/odstudio/ourdiet/ui/food/FoodFragment.kt
@@ -1,17 +1,21 @@
 package com.odstudio.ourdiet.ui.food
 
 import android.app.DatePickerDialog
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.EditText
 import android.widget.ExpandableListView
 import androidx.fragment.app.Fragment
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.odstudio.ourdiet.R
+import com.odstudio.ourdiet.RecordingActivity
+import kotlinx.android.synthetic.main.fragment_food.*
 import java.text.SimpleDateFormat
 import java.util.*
 import kotlin.collections.ArrayList
@@ -122,6 +126,13 @@ class FoodFragment : Fragment() {
         )
         val adapter = FoodAdapter(activity, meal, food)
         elv.setAdapter(adapter)
+
+        var btn_addrecord = root.findViewById<Button>(R.id.btn_addfood)
+        btn_addrecord.setOnClickListener {
+            val intent = Intent(activity, RecordingActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+            startActivity(intent)
+        }
 
         return root
     }
